25f9fd14e535f7f291f03be7b63027e5db37caed
ShamimHossain1 <beastb697@gmail.com>
2023-04-16 16:29:17 +0600

    github login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,42 +1,64 @@
 import { Button } from 'flowbite-react';
 import React, { useState } from 'react';
 import app from '../../Firebase/Firebase.init';
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from 'firebase/auth';
 
 
 const Login = () => {
     const [user, setUser] = useState(null)
     const auth = getAuth(app);
-    // console.log(app)
-    const provider = new GoogleAuthProvider();
+    console.log(user)
+    const GoogleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider()
 
-    const handleGoogleSignIn= () =>{
-        signInWithPopup(auth, provider)
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, GoogleProvider)
+            .then(result => {
+                const LoggedInUser = result.user;
+                setUser(LoggedInUser);
+                // console.log(LoggedInUser);
+            })
+            .catch(error => {
+                console.log('error:', error.message)
+            })
+    }
+
+
+    const handleGitHubSignIn = () => {
+        signInWithPopup(auth, githubProvider)
         .then(result =>{
-            const LoggedInUser = result.user;
-            setUser(LoggedInUser);
-            console.log(LoggedInUser);
+            const loggedUser = result.user;
+            console.log(loggedUser)
+            setUser (loggedUser)
         })
         .catch(error => {
-            console.log('error:', error.message )
+            console.log('error:', error.message)
         })
     }
-    const handleGoogleSignOut= () =>{
+
+    const handleGoogleSignOut = () => {
         signOut(auth)
-        .then(result=>{
-            console.log(result);
-            setUser(null)
-        })
-        .catch(error=>{
-            console.log(error)
-        })
+            .then(result => {
+                console.log(result);
+                setUser(null)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
+
     return (
         <div className='flex items-center flex-col gap-5'>
             {/* user ? signOut : sign In */}
-          { user ?  <Button className='96' onClick={handleGoogleSignOut}>Sign Out</Button> :
-            <Button className='96' onClick={handleGoogleSignIn}>Google Login</Button>}
-            { user && <div>
+            {
+                user ?
+                    <Button className='' onClick={handleGoogleSignOut}>Sign Out</Button> :
+                    <div>
+                        <Button className='mb-10' onClick={handleGoogleSignIn}>Google Login</Button>
+                        <Button className='' onClick={handleGitHubSignIn}>GitHub Login</Button>
+                    </div>
+            }
+            {user && <div>
                 <h3>User : {user.displayName}</h3>
                 <h3>User Email : {user.email}</h3>
                 <img src={user.photoURL} alt="" />
--- src/__steps.js
+++ src/__steps.js
@@ -1,16 +1,38 @@
 /**
+ * 
+ * -----------------------------
+ *         INITIAL SETUp
+ * ---------------------------------
+ * 
  * 1. visit : console.firebase.google.com
  * 2. create project (skip google analytics)
  * 3.register app (create config)
  * 4. install firebase: npm install firebase
  * 5. add config file to your project
  * 6. Do not publish or make firebase config to public by pushing  those to github
+ * 
+ * ----------------------------------------------------
+ *                   INTEGRATION
+ * --------------------------------------------------------------
+ * 
+ * 
  * 7.Visit: go to docs> build > authentication> web > get started
  * 8. export app from the firebase.config.js file : exp[ort default app
  * 9. Login.jsx: import getAuth from firebase/auth
  * 10. create const auth = getAuth(app)
+ * 
+ * -----------------------------------------------
+ *                  Provider setup
+ * ----------------------------------------------
+ * 
  * 11. Import a google auth provider and create a new provider
  * 12. use signInWithPopUp and pass auth and provider
  * 13. activate sign-in method ( google, facebook, github)
- * 14. 
+ * 14. [vite]: change 127.0.0.1 to localhost
+ * 
+ * 
+ * -----------------------------------------
+ *          more Auth provider
+ * ----------------------------------------
+ * 1. Activate the auth provider (create app, provider redirect url, client id , client secret)
  */
